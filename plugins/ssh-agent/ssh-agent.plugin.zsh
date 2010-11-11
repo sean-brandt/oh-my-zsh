7a754fbb01954c7c1c80d5fa04092cedcca58835
Sean Brandt <sean@fuzzymagic.com>
2010-11-10 21:00:07 -0500

    ssh darwin keychain support

--- plugins/ssh-agent/ssh-agent.plugin.zsh
+++ plugins/ssh-agent/ssh-agent.plugin.zsh
@@ -1,13 +1,17 @@
 # Based on code from Joseph M. Reagle
 # http://www.cygwin.com/ml/cygwin/2001-06/msg00537.html
 
+
 local SSH_ENV=$HOME/.ssh/environment
+local OS=`uname`
+local SSH_ADD_ARGS=""
+[[ "$OS" == "Darwin" && "$SSH_AGENT_FROM_KEYCHAIN" == "1" ]] && local SSH_ADD=" -k"
 
 function start_agent {
   /usr/bin/env ssh-agent | sed 's/^echo/#echo/' > ${SSH_ENV}
   chmod 600 ${SSH_ENV}
   . ${SSH_ENV} > /dev/null
-  /usr/bin/ssh-add;
+  /usr/bin/ssh-add${SSH_ADD_ARGS};
 }
 
 # Source SSH settings, if applicable
