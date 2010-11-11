2a3102eb00d1eb090ae9aa063e4ff1e4ce7b3407
Sean Brandt <sean@fuzzymagic.com>
2010-11-10 20:19:02 -0500

    add rvm plugin

--- plugins/rvm/rvm.plugin.zsh
+++ plugins/rvm/rvm.plugin.zsh
@@ -1,3 +1,4 @@
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session
 fpath=($rvm_path/scripts/zsh/Completion $fpath)
 
 alias rubies='rvm list rubies'
