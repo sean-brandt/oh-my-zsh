05ca5d0b20ab885c2132093d01582109640cf854
Sean Brandt <sean@fuzzymagic.com>
2010-11-30 18:23:21 -0500

    fix up ssh add for darwin

--- plugins/ssh-agent/ssh-agent.plugin.zsh
+++ plugins/ssh-agent/ssh-agent.plugin.zsh
@@ -2,19 +2,20 @@
 # http://www.cygwin.com/ml/cygwin/2001-06/msg00537.html
 
 
+set -o xtrace
 # We're being passed an agent socket
 [ -n "$SSH_CLIENT" -a -n "$SSH_AUTH_SOCK" ] && return 0
 
 local SSH_ENV=$HOME/.ssh/environment
 local OS=`uname`
 local SSH_ADD_ARGS=""
-[[ "$OS" == "Darwin" && "$SSH_AGENT_FROM_KEYCHAIN" == "1" ]] && local SSH_ADD=" -k"
+[[ "$OS" == "Darwin" && "$SSH_AGENT_FROM_KEYCHAIN" == "1" ]] && local SSH_ADD_ARGS="-k"
 
 function start_agent {
   /usr/bin/env ssh-agent | sed 's/^echo/#echo/' > ${SSH_ENV}
   chmod 600 ${SSH_ENV}
   . ${SSH_ENV} > /dev/null
-  /usr/bin/ssh-add${SSH_ADD_ARGS};
+  /usr/bin/ssh-add ${SSH_ADD_ARGS};
 }
 
 # Source SSH settings, if applicable
@@ -29,3 +30,4 @@ else
 fi
 
 
+set +o xtrace
