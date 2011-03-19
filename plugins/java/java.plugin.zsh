046538d966f9505bbb1bfc7b3cf6cd22941237dd
Sean Brandt <sean@fuzzymagic.com>
2011-03-19 07:57:48 -0400

    updates to default to java 1.7

--- plugins/java/java.plugin.zsh
+++ plugins/java/java.plugin.zsh
@@ -1,7 +1,5 @@
 local OS=`uname`
-[[ "$OS" == "Darwin" && -x /Library/Java/Home && -z "$JAVA_HOME" ]] && export JAVA_HOME="$(/usr/libexec/java_home)"
+JAVA_VERSION="${JAVA_VERSION:=1.7}"
+[[ "$OS" == "Darwin" && -z "$JAVA_HOME" ]] && export JAVA_HOME="$(/usr/libexec/java_home --version $JAVA_VERSION)"
 
 path=($JAVA_HOME/bin $path)
-
-
-
