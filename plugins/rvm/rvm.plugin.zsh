cff50ad8a44830a284523e85c835e231a7b71285
Sean Brandt <sean@fuzzymagic.com>
2012-06-22 08:45:46 -0400

    remove custom adds to rvm plugin

--- plugins/rvm/rvm.plugin.zsh
+++ plugins/rvm/rvm.plugin.zsh
@@ -1,17 +1,3 @@
-function install-rvm() {
-    bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
-}
-
-##
-# Setup RVM
-##
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session
-
-# add rvm completion function to path
-fpath=("$HOME/.rvm/scripts/zsh/Completion" $fpath)
-autoload -U compinit
-compinit -i
-
 alias rubies='rvm list rubies'
 alias gemsets='rvm gemset list'
 
