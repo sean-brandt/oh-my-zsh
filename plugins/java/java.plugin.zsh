d2468be99a8e8b6c190ddcc074fc5f1e2fe232b9
Sean Brandt <sbrandt@geico.com>
2013-01-17 10:29:50 -0500

    add in switchJava func

--- plugins/java/java.plugin.zsh
+++ plugins/java/java.plugin.zsh
@@ -1,5 +1,15 @@
 local OS=`uname`
 JAVA_VERSION="${JAVA_VERSION:=1.7}"
-[[ "$OS" == "Darwin" && -z "$JAVA_HOME" ]] && export JAVA_HOME="$(/usr/libexec/java_home --version $JAVA_VERSION)"
+[[ "$OS" == "Darwin" && -z "$JAVA_HOME" ]] && export JAVA_HOME="$(/usr/libexec/java_home -v $JAVA_VERSION)"
 
 path=($JAVA_HOME/bin $path)
+
+
+function switchJava() {
+  old_java_home=$JAVA_HOME
+  export JAVA_VERSION=$1
+  export JAVA_HOME=$(/usr/libexec/java_home -v $JAVA_VERSION)
+  PATH=$(echo $PATH | perl -pi -e "s@(:)?$old_java_home/bin(:)?@@" | perl -pi -e 's@::@:@g')
+  export PATH=$JAVA_HOME/bin:$PATH
+  echo "New JAVA_HOME=$JAVA_HOME; new PATH=$PATH"
+}
