5e17e2b3aa0dd3f59064711629ec5f290c2b1c55
Sean Brandt <sean@fuzzymagic.com>
2013-12-16 20:59:53 -0500

    Revert "add rvm plugin"
    This reverts commit 2a3102eb00d1eb090ae9aa063e4ff1e4ce7b3407.

--- plugins/rvm/rvm.plugin.zsh
+++ plugins/rvm/rvm.plugin.zsh
@@ -1,4 +1,3 @@
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session
 fpath=($rvm_path/scripts/zsh/Completion $fpath)
 
 alias rubies='rvm list rubies'
